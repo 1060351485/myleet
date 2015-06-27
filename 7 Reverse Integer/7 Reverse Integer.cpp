703d3f574246a6c920f066de4b99972df96a4fd3
Apple <1060351485@qq.com>
2015-06-27 13:29:35 +0800

    8ms

--- 7 Reverse Integer/7 Reverse Integer.cpp
+++ 7 Reverse Integer/7 Reverse Integer.cpp
@@ -1,16 +1,19 @@
 //
 //  7 Reverse Integer.cpp
-//  Reverse Integer
+//  
 //
-//  Created by Apple on 15/6/26.
-//  Copyright (c) 2015年 hjh. All rights reserved.
+//  Created by Apple on 15/6/27.
 //
+//
+
 class Solution {
 public:
     int reverse(int x) {
         
         bool neg = false;
         if ( x < 0 ){
+            if ( x <= INT32_MIN )
+                return 0;
             neg = true;
             x = -x;
         }
@@ -25,9 +28,8 @@ public:
         if ( reg > 0x7fffffff) // overflow check
             return 0;
         if ( neg )
-            return (int)reg;
-        else
             return (-1)*(int)reg;
+        else
+            return (int)reg;
     }
-    
 };
