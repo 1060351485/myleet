03349621b24ef68a48d84e7e9ff9238f195b0db5
Apple <1060351485@qq.com>
2015-07-15 19:54:49 +0800

    shorter

---
+++ 237 Delete Node in a Linked List/237 Delete Node in a Linked List-faster.cpp
@@ -0,0 +1,23 @@
+//
+//  237 Delete Node in a Linked List-faster.cpp
+//  
+//
+//  Created by Apple on 15/7/15.
+//
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next?node->next->next:NULL;
+    }            
+};
