60db670a8dae728ed3924c89eb23a592e712d459
Apple <1060351485@qq.com>
2015-07-17 12:01:55 +0800

    add a new solve

--- 165 Compare Version Numbers/165 Compare Version Numbers.cpp
+++ 165 Compare Version Numbers/165 Compare Version Numbers.cpp
@@ -29,4 +29,26 @@ public:
         }
         return 0;
     }
+    
+    
+    // move last lines into loop
+    int compareVersion(string version1, string version2) {
+        int ver1 = 0, ver2 = 0;
+        int i = 0, j = 0;
+        int m = version1.length(), n = version2.length();
+        while( (i < m)||(j < n) ){
+            while(isdigit(version1[i])&&(i<m))
+                ver1 = ver1*10 + version1[i++]-'0';
+            while(isdigit(version2[j])&&(j<n))
+                ver2 = ver2*10 + version2[j++]-'0';
+            if(ver1 < ver2)
+                return -1;
+            if(ver1 > ver2)
+                return 1;
+            ver1 = 0, ver2 = 0;
+            i++;
+            j++;
+        }
+        return 0;
+    }
 };
