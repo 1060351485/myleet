1ff04ae4d2b59dcb230db32c8b03572e677ee1b9
Apple <1060351485@qq.com>
2015-07-17 11:00:49 +0800

    add explain

--- 228 Summary Range/228 Summary Range-better.cpp
+++ 228 Summary Range/228 Summary Range-better.cpp
@@ -15,6 +15,7 @@ public:
         int i = 0,j = 1;
         while(i < size){
             while((j < size)&&(nums[j-1] == nums[j]-1)) j++;
+            // instead of cmp values, cmp index, in case of overflow
             if(i < j-1) //  essential!!
                 V.push_back(to_string(nums[i])+"->"+to_string(nums[j-1]));
             else
