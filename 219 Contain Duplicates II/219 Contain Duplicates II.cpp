49e94515f5cd590078a5384efdb9412eecb52c67
Apple <1060351485@qq.com>
2015-07-17 20:42:31 +0800

    32ms

---
+++ 219 Contain Duplicates II/219 Contain Duplicates II.cpp
@@ -0,0 +1,26 @@
+//
+//  219 Contain Duplicates II.cpp
+//  
+//
+//  Created by Apple on 15/7/17.
+//
+//
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> M;
+        int mark = 0;
+        for(int i = 0 ;i < nums.size(); i++){
+            if(M.find(nums[i]) == M.end())
+                M[nums[i]] = i;
+            else{
+                mark = M[nums[i]];   //mark is old index, M[nums[i]] is the latest index
+                M[nums[i]] = i;
+                if(i-mark <= k)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
