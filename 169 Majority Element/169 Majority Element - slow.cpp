40f30cab15f2d33c8351aa176e5bf13a34202e26
Apple <1060351485@qq.com>
2015-07-19 16:47:31 +0800

    52ms - slow

---
+++ 169 Majority Element/169 Majority Element - slow.cpp
@@ -0,0 +1,31 @@
+//
+//  169 Majority Element - slow.cpp
+//  
+//
+//  Created by Apple on 15/7/19.
+//
+//
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        const int Size = nums.size();
+        int memo[Size] = {0};
+        unordered_map<int, int> M;
+        memo[0] = nums[0];
+        M[nums[0]] = 1;
+        for( int i = 1; i< nums.size() ;i++ ){
+            if(M.find(nums[i]) == M.end()){
+                M[nums[i]] = 1;
+                memo[i] = memo[i-1];
+            }else{
+                M[nums[i]]++;
+                if(M[nums[i]] > (i+1)/2)
+                    memo[i] = nums[i];
+                else
+                    memo[i] = memo[i-1];
+            }
+        }
+        return memo[Size-1];
+    }
+};
