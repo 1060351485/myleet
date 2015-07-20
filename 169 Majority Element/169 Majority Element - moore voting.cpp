0cbfc8f0d5fd5578d6aace4867f938d2c254017f
Apple <1060351485@qq.com>
2015-07-20 10:09:08 +0800

    16ms

--- 169 Majority Element/169 Majority Element - moore voting.cpp
+++ 169 Majority Element/169 Majority Element - moore voting.cpp
@@ -6,4 +6,21 @@
 //
 //
 
-#include <stdio.h>
+class Solution {
+public:
+    // moore voting
+    int majorityElement(vector<int>& nums) {
+        int rslt = 0, count = 0;
+        for( int i = 0; i < nums.size() ; i++ ){
+            if((rslt == 0)||(rslt == nums[i])){
+                rslt = nums[i];
+                count++;
+            }else{
+                count--;
+                if(count == 0)
+                    rslt = 0;
+            }
+        }
+        return rslt;
+    }
+};
