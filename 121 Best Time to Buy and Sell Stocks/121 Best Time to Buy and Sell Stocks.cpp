510e69cab9d1e5a67bdcfcd9476559178de45311
Apple <1060351485@qq.com>
2015-07-23 19:19:00 +0800

    add explain

--- 121 Best Time to Buy and Sell Stocks/121 Best Time to Buy and Sell Stocks.cpp
+++ 121 Best Time to Buy and Sell Stocks/121 Best Time to Buy and Sell Stocks.cpp
@@ -8,15 +8,22 @@
 
 class Solution {
 public:
+    
+    // O(2n)
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         if(!n) return 0;
         
         int *minPrs = new int[n];
         int *maxPrs = new int[n];
-        minPrs[0] = prices[0];
-        maxPrs[n-1] = prices[n-1];
+        minPrs[0] = prices[0];  // minPrs[i] is min from index 0 to i
+        maxPrs[n-1] = prices[n-1]; // maxPrs[i] is max from index i to n-1
         
+        // all we need is the min buy in time and max sell time,
+        // and buy in time <= sell time
+        // all n prices is divided by index i,
+        // find min value in prices[0] to prices[i-1]
+        // and max value in prices[i] to prices[n-1]
         for( int i = 1; i < n ; i++){
             minPrs[i] = min(minPrs[i-1], prices[i]);
             maxPrs[n-i-1] = max(maxPrs[n-i], prices[n-i-1]);
@@ -24,6 +31,22 @@ public:
         int rslt = INT32_MIN;
         for(int i = 0; i < n ;i++)
             rslt = max(maxPrs[i] - minPrs[i], rslt);
+        
+        return rslt;
+    }
+    
+    
+    // simplify, O(1*n)
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(!n) return 0;
+        
+        int minPrs = prices[0];
+        int rslt = 0;
+        for( int i = 1; i < n ; i++){
+            minPrs = min(minPrs, prices[i]);
+            rslt = max(rslt, prices[i] - minPrs);
+        }
         
         return rslt;
     }
