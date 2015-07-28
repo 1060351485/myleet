49a4db53af24decd66376c3031bd66ec33e17860
Apple <1060351485@qq.com>
2015-07-28 19:35:21 +0800

    all a solu

---
+++ 3 Longest Substring Without Repeating Characters/3 Longest Substring Without Repeating Characters - faster.cpp
@@ -0,0 +1,8 @@
+//
+//  3 Longest Substring Without Repeating Characters - faster.cpp
+//  
+//
+//  Created by Apple on 15/7/28.
+//
+//
+
--- 3 Longest Substring Without Repeating Characters/3 Longest Substring Without Repeating Characters.cpp
+++ 3 Longest Substring Without Repeating Characters/3 Longest Substring Without Repeating Characters.cpp
@@ -8,9 +8,8 @@
 
 class Solution {
 public:
+    // 60 ms
     int lengthOfLongestSubstring(string s) {
-        if(s.length() < 2)
-            return s.length();
         int start = 0, maxCnt = 0;
         // start from i, end at j
         unordered_map<int, int> m;
@@ -20,6 +19,25 @@ public:
                     start = m[s[i]] + 1;
             }
             m[s[i]] = i;
+            maxCnt = max(i-start+1, maxCnt);
+        }
+        return maxCnt;
+    }
+    
+    
+    // use vector instead of hash_map is faster
+    // 16 ms
+    int lengthOfLongestSubstring(string s) {
+        int start = 0, maxCnt = 0;
+        // start from i, end at j
+        // unordered_map<int, int> m;
+        vector<int> v(256, -1);
+        for(int i = 0; i < s.length() ; i++){
+            if(v[s[i]] != -1){
+                if(v[s[i]] >= start)
+                    start = v[s[i]] + 1;
+            }
+            v[s[i]] = i;
             maxCnt = max(i-start+1, maxCnt);
         }
         return maxCnt;
