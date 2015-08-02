ec72eed395b653ce65c4d852752ecf45e3b54800
Apple <1060351485@qq.com>
2015-08-02 13:01:55 +0800

    3 solu 4ms

--- 153 Find Minimum in Rotated Sorted Array/153 Find Minimum in Rotated Sorted Array.cpp
+++ 153 Find Minimum in Rotated Sorted Array/153 Find Minimum in Rotated Sorted Array.cpp
@@ -8,6 +8,8 @@
 
 class Solution {
 public:
+    
+    // O(n)
     int findMin(vector<int>& nums) {
         if(nums.empty()) return 0;
         for(int i = 1 ;i < nums.size(); i++){
@@ -17,4 +19,35 @@ public:
         }
         return nums[0];
     }
+    
+    // binary search, O(logn)
+    int findMin(vector<int>& nums) {
+        int start = 0, end = nums.size()-1, mid;
+        if(nums[start] < nums[end]) return nums[start];  // if not rotated, return first one
+        while(start != end){
+            mid = (start + end)>>1;
+            if(nums[mid] < nums[end])
+                end = mid;
+            else
+                start = mid+1;
+        }
+        return nums[start];
+        
+    }
+    
+    //better O(logn)
+    int findMin(vector<int>& nums) {
+        int start = 0, end = nums.size()-1, mid;
+        while(start != end){
+            if(nums[start] < nums[end])
+                return nums[start];  // if not rotated, return first one
+            mid = (start + end)>>1;
+            if(nums[mid] < nums[end])
+                end = mid;
+            else
+                start = mid+1;
+        }
+        return nums[start];
+    }
+
 };
