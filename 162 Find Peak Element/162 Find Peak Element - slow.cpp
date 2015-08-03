f24b37fd1fa28a58d0444375d291666313ed7886
Apple <1060351485@qq.com>
2015-08-03 22:42:00 +0800

    8ms - slow

--- 162 Find Peak Element/162 Find Peak Element - slow.cpp
+++ 162 Find Peak Element/162 Find Peak Element - slow.cpp
@@ -9,11 +9,13 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums){
-        for(int i = 0; i < (int)nums.size()-2;i++){
+        int n = nums.size(); //!!!! unsigned long !!! overflow after minus 2
+        for(int i = 0; i < n-2;i++){
             if((nums[i] < nums[i+1])&&(nums[i+1] > nums[i+2]))
                 return i+1;
         }
         return nums[0]>nums[nums.size()-1]?0:nums.size()-1;
         
     }
+    
 };
